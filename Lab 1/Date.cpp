2e815e7f98668b9040dd47f32d36bd9701c797da
Andrew Kozak <Canooka143@hotmail.com>
2019-09-09 09:21:52 -0800

    Adding Lab2 part a to the repo

---
+++ Lab 1/Date.cpp
@@ -0,0 +1,23 @@
+/**
+* Lab01.cpp
+* Andrew Kozak
+* 09/04/2019
+* This program asks the user a number then returns it squared
+*/
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+
+int main()
+{
+	int x; //given number
+	cout << "Type an integer"; //Asks for number
+	cin >> x; //Grabs number from user
+	x = x + 1; //Adds 1 to the user's number
+	cout << (x * x) << "is" << x << "squared" << endl; //Outputs the final number
+
+	return 0;
+}
