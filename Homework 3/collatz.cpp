d5ff66707b889bde5c342ad21a826e8c01f34cad
Andrew Kozak <Canooka143@hotmail.com>
2019-09-25 09:30:17 -0800

    Created collatz.cpp and created the program in homework 3

---
+++ Homework 3/collatz.cpp
@@ -0,0 +1,38 @@
+
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	int x = 0;
+
+	cout << "Enter a number to be collatzed: ";
+	cin >> x;
+
+	while (x < 1)
+	{
+		cout << "Please enter a positive integer: ";
+		cin >> x;
+	}
+
+	cout << "The collatz sequence is: " << x << ", ";
+	while (x > 1)
+	{
+		if (x % 2 == 1)
+		{
+			x = 3 * x + 1;
+		}
+		else
+		{
+			x = x / 2; 
+		}
+
+		cout << x << ", ";
+	}
+	
+
+	return 0; 
+	}
