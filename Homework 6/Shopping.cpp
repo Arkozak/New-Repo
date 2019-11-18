c10cec42a67371fd8f3105c5500fa66aea678db4
Andrew <arkozak@alaska.edu>
2019-11-17 16:46:45 -0900

    created Shopping.cpp and created the main function, and maps

---
+++ Homework 6/Shopping.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <iomanip>
+#include <string>
+
+using std::cout;
+using std::map;
+using std::string;
+using std::getline;
+using std::string;
+using std::cin;
+
+struct item
+{
+	double cost;
+	int units;
+};
+
+int main()
+{
+	int x = 0;
+	int units = 6;
+	std::map<std::string, item> products
+	{
+		{"Milk", {4.30, units}},
+		{"Tea", {4.30, units}},
+		{"Onion", {4.30, units}},
+		{"Chocolate", {4.30, units}},
+		{"Goldfish", {4.30, units}}
+	};
+
+	map<string, int> shoppingcart;
+	
+	while (x != 4)
+	{
+		cout << "Would you like to view cart(0), ";
+		cout << "add item(1), remove item(2), pay(3), or exit(4) ";
+		cin >> x;
+		
+		if (x == 0)
+		{
+		
+		}
+		else if (x == 1)
+		{
+
+		}
+		else if (x == 2)
+		{
+
+		}
+		else if (x == 3)
+		{
+
+		}
+		else if (x == 4)
+		{
+
+		}
+		else
+		{
+			cout << "Input invalid\n";
+		}
+	}
+}
