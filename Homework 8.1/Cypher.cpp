9691fd7a23314d01c3cd2a206f0e3e11cdc1c3ab
Andrew <arkozak@alaska.edu>
2019-12-08 20:34:46 -0900

    Added in the bull cow code and added an actual random number generator

---
+++ Homework 8.1/Cypher.cpp
@@ -0,0 +1,107 @@
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <string>
+using std::string;
+#include <vector>
+using std::vector;
+#include <random>
+using std::random_device;
+using std::mt19937;
+using std::uniform_int_distribution;
+
+int main()
+{
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<int> distrib1(1, 2);
+
+	vector<int> hidden;
+	string guess;
+	int x = 0;
+
+	int bulls = 0;
+	int cows = 0;
+
+	for (auto i = 0; i < 4; i++) //Creating hidden vector
+	{
+		hidden.push_back(distrib1(gen));
+		cout << hidden[i];
+	}
+	
+
+	while (true)
+	{
+		cout << "Guess a 4 digit number: " << endl; //grabbing guess
+		cin >> guess;
+
+		for (auto i = 0; i < 4; i++)
+		{
+			switch (guess[i]) //first number
+			{
+			case '0':
+				x = 0;
+				break;
+			case '1':
+				x = 1;
+				break;
+			case '2':
+				x = 2;
+				break;
+			case '3':
+				x = 3;
+				break;
+			case '4':
+				x = 4;
+				break;
+			case '5':
+				x = 5;
+				break;
+			case '6':
+				x = 6;
+				break;
+			case '7':
+				x = 7;
+				break;
+			case '8':
+				x = 8;
+				break;
+			case '9':
+				x = 9;
+				break;
+			}
+
+			for (auto j = 0; j < 4; j++)
+			{
+				if (x == hidden[j])
+				{
+					if (i == j)
+					{
+						bulls++;
+					}
+					else
+					{
+						cows++;
+					}
+				}
+
+			}
+		}
+
+		cout << "Bulls: " << bulls << endl;
+		cout << "Cows: " << cows << endl;
+		if (bulls == 4)
+		{
+			cout << "You win!";
+			return 0;
+		}
+		bulls = 0;
+		cows = 0;
+	}
+
+
+	return 0;
+}
+
